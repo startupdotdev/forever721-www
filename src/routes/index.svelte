a3dd07cfb545f24d12004043b869ee14c888feeb
John Mosesman <johnmosesman@gmail.com>
2022-02-17 16:47:59 -0600

    Remove boilerplate content and put rough content in place for home page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,59 +1,84 @@
-<script context="module" lang="ts">
-	export const prerender = true;
-</script>
-
 <script lang="ts">
-	import Counter from '$lib/Counter.svelte';
+	let openseaUrl: string;
+
+	async function evaluateNft(openseaUrl: string): Promise<void> {
+		console.log("boop");
+
+	}
 </script>
 
 <svelte:head>
-	<title>Home</title>
+	<title>Forever721</title>
 </svelte:head>
 
-<section>
-	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</div>
+<div class="m-4">
+	<!-- placeholder image -->
+	<img src="https://upload.wikimedia.org/wikipedia/en/0/07/Cryptopunks_general.jpg" />
 
-		to your new<br />SvelteKit app
-	</h1>
+	<div>
+		<h1>Forever721 NFT Analysis</h1>
+		<h2>NFTs are permanent. Sometimes their contents are not.</h2>
+		<p>Know exactly what's inside your NFT, and permanent it really is.</p>
 
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
+		<button>Install Forever721 browser extension</button>
+		<button>Or try it out below</button>
 
-	<Counter />
-</section>
+	</div>
 
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 1;
-	}
+	<!-- placeholder break -->
+	<hr/>
 
-	h1 {
-		width: 100%;
-	}
+	<div>
+		<h2>Scan an NFT</h2>
 
-	.welcome {
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
+		<!-- placeholder image -->
+		<img src="https://upload.wikimedia.org/wikipedia/en/0/07/Cryptopunks_general.jpg" />
 
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+		<!-- todo make component -->
+		<form on:submit|preventDefault={() => evaluateNft('hi', 'hi') }>
+			<input
+				class="rounded-tr-none rounded-br-none"
+				style="border-bottom-right-radius: 0px; border-top-right-radius: 0px;"
+				value={openseaUrl}
+			/>
+
+			<hr/>
+
+			<p>NFT Contract Address</p>
+			<input
+				class="rounded-tr-none rounded-br-none"
+				style="border-bottom-right-radius: 0px; border-top-right-radius: 0px;"
+				value={openseaUrl}
+			/>
+
+			<p>Token ID</p>
+			<input
+				class="rounded-tr-none rounded-br-none"
+				style="border-bottom-right-radius: 0px; border-top-right-radius: 0px;"
+				value={openseaUrl}
+			/>
+
+			<input
+				class:disabled={true}
+				class="block border border-white p-2"
+				type="submit"
+				value="Analyze NFT"
+			/>
+		</form>
+	</div>
+
+	<div>
+		<h2>What if your BAYC became a 💩 emoji?</h2>
+		<p>NFT ownership is permanently recorded on the blockchain.</p>
+		<p>However, for some NFTs, the metadata that we all recognize and love (the image, name, rarity attributes) could change at any time. It all depends on how the developers’ setup the NFTs.</p>
+	</div>
+
+	<div>
+		<h2>How does Forever721 work?</h2>
+		<p>The Forever721 browser extension shows you exactly what is in the NFT you see while browsing OpenSea.</p>
+		<p>See our letter grade evaluation alongside with the durability of that NFT in real time.</p>
+
+		<!-- placeholder image -->
+		<img src="https://upload.wikimedia.org/wikipedia/en/0/07/Cryptopunks_general.jpg" />
+	</div>
+</div>
